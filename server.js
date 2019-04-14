ca06742216b6f8dcd87b7e85d0656ebb61f2e3dd
Emmanuel Mark <info@emmanuelmark.guru>
2019-04-13 22:37:25 -0400

    Created create-event API method

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var Redis = require('ioredis');
 
-//var redis = new Redis('6379');
+var redis = new Redis('6379');
 
 var bodyParser = require('body-parser');
 
@@ -15,10 +15,38 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 5000;
 
 app.post('/api/create-event', function(req, res) {
-  var eventDetails = {
-    eventName: req.body.eventName
+  var timeSlotData = [];
+
+  var eventDuration = moment.duration(moment(req.body.eventEndDate, 'HH:mm DD MM YYYY').diff(moment(req.body.eventStartDate, 'HH:mm DD MM YYYY')));
+
+  var timeSlotDuration = eventDuration.asMinutes() / req.body.numberOfSlots;
+
+  for (let i = 0; i < req.body.numberOfSlots; i++) {
+    timeSlotData.push({
+      slotStartDate: moment(req.body.eventStartDate, 'HH:mm DD MM YYYY').add(timeSlotDuration * i, 'minutes').format('HH:mm DD MM YYYY'),
+      slotEndDate: moment(req.body.eventStartDate, 'HH:mm DD MM YYYY').add(timeSlotDuration * (i + 1), 'minutes').format('HH:mm DD MM YYYY'),
+      availableVolunteerSlots: req.body.availableVolunteerSlots,
+      volunteerSlotsTaken: 0
+    });
   }
-  res.json({ eventName: eventDetails.eventName})
+
+  var eventDetails = {
+    eventName: req.body.eventName,
+    id: req.body.eventName.toLowerCase(),
+    eventStartDate: req.body.eventStartDate,
+    eventEndDate: req.body.eventEndDate,
+    numberOfSlots: req.body.numberOfSlots,
+    availableVolunteerSlots: req.body.availableVolunteerSlots,
+    timeSlotData: timeSlotData,
+  };
+  
+  redis.get('events', function(error, result){
+    var currentEvents = JSON.parse(result);
+    currentEvents.push(eventDetails);
+    redis.set('events', JSON.stringify(currentEvents));
+  });
+
+  res.json({ 'event': eventDetails });
 });
 
 app.listen(port, function() {
