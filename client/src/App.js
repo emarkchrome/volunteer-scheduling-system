b82e01ca53c5765d5c1149c8527611a7233cd382
Emmanuel Mark <info@emmanuelmark.guru>
2019-04-14 23:45:26 -0400

    Create event creation form

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,47 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    /*fetch('/api/create-event', {
+  constructor() {
+    super();
+    this.state = { events: [], eventName: null, eventStartDate: null, eventEndDate: null, numberOfSlots: null, availableVolunteerSlots: null };
+  }
+  componentWillMount() {
+    var thisInstance = this;
+
+    fetch('/api/get-events', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(function(data) {
+        return data.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        thisInstance.setState({ events: data.events });
+      });
+  }
+  handleInputChange(e) {
+    const name = e.target.name;
+    this.setState({
+      [name]: e.target.value
+    });
+  }
+  createEvent(e) {
+
+    e.preventDefault();
+
+    var thisInstance = this;
+
+    fetch('/api/create-event', {
       method: 'POST',
       body: JSON.stringify({
-        'eventName': 'Fundraiser',
-        'eventStartDate': '14:00 24 05 2019',
-        'eventEndDate': '19:00 24 05 2019',
-        'numberOfSlots': 5,
-        'availableVolunteerSlots': 4
+        'eventName': thisInstance.state.eventName,
+        'eventStartDate': thisInstance.state.eventStartDate,
+        'eventEndDate': thisInstance.state.eventEndDate,
+        'numberOfSlots': thisInstance.state.numberOfSlots,
+        'availableVolunteerSlots': thisInstance.state.availableVolunteerSlots
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -23,19 +55,10 @@ class App extends Component {
       })
       .then(function(data) {
         console.log(data);
+        window.location.reload();
       });
 
-  fetch('/api/get-events', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(function(data) {
-      return data.json();
-    })
-    .then(function(data) {
-      console.log(data);
+  }
   joinTimeslot(e) {
     fetch('/api/join-timeslot', {
       method: 'POST',
@@ -88,9 +111,23 @@ class App extends Component {
               )
             })
           }
-        </header>
-        <div className="events"></div>
+          {
+            this.state.events.length == 0 && (
+              <h3>You haven't made any events. Try making one to see it appear here.</h3>
+            )
+          }
+        </div>
         <div className="create-event-form">
+          <hr></hr>
+          <h2>Create Event:</h2>
+          <form onSubmit={this.createEvent.bind(this)}>
+            <input required type="text" placeholder="Event Name" name="eventName" onChange={this.handleInputChange.bind(this)} /><br /><br />
+            <input required type="datetime-local" placeholder="Event Start Time" name="eventStartDate" onChange={this.handleInputChange.bind(this)} /><br /><br />
+            <input required type="datetime-local" placeholder="Event End Time" name="eventEndDate" onChange={this.handleInputChange.bind(this)} /><br /><br />
+            <input required type="number" placeholder="Into how many equally sized timeslots do you want to split the event?" name="numberOfSlots" onChange={this.handleInputChange.bind(this)} /><br /><br />
+            <input required type="number" placeholder="How many volunteers should be able to signup for each timeslot?" name="availableVolunteerSlots" onChange={this.handleInputChange.bind(this)} /><br /><br />
+            <input required type="submit" value="Create Event"></input>
+          </form>
         </div>
       </div>
     );
